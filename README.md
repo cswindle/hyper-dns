eff2d35c31ed1dfb4428ecb012e9670838b95678
Chris Swindle <christopher.swindle@metaswitch.com>
2017-06-08 11:58:36 +0100

    Some fixes and adding basic info on how to use the library.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # hyper-dns
-NetworkConnector to allow using a custom DNS server in Hyper
+NetworkConnector to allow using a custom DNS server with SRV records in Hyper.
+
+Below shows a quick example of how this library can be used (you will need to have SRV records setup for the domain to query):
+
+```
+extern crate hyper_dns;
+extern crate hyper;
+
+use std::net::ToSocketAddrs;
+
+fn main() {
+
+    let dns_addr: std::net::SocketAddr = ("127.0.0.1", 8600).to_socket_addrs().unwrap().next().unwrap();
+
+    let client = hyper::client::Client::with_connector(
+        hyper_dns::DnsConnector::new(dns_addr, hyper::net::HttpConnector));
+
+    client.get("http://test.service.consul/").send();
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -7,15 +7,16 @@ extern crate trust_dns;
 
 use trust_dns::client::ClientHandle;
 use rand::Rng;
+use hyper::net::{NetworkConnector, NetworkStream};
 
 /// A connector that wraps another connector and provides custom DNS resolution.
 #[derive(Debug, Clone)]
-pub struct DnsConnector<C: hyper::net::NetworkConnector> {
+pub struct DnsConnector<C: NetworkConnector> {
     connector: C,
     dns_addr: std::net::SocketAddr,
 }
 
-impl <C: hyper::net::NetworkConnector> DnsConnector<C> {
+impl<C: NetworkConnector> DnsConnector<C> {
     pub fn new(dns_addr: std::net::SocketAddr, connector: C) -> DnsConnector<C> {
 
         DnsConnector {
@@ -25,8 +26,8 @@ impl <C: hyper::net::NetworkConnector> DnsConnector<C> {
     }
 }
 
-impl <C: hyper::net::NetworkConnector<Stream=S>, S: hyper::net::NetworkStream + Send> hyper::net::NetworkConnector for DnsConnector<C> {
-
+impl<C: NetworkConnector<Stream = S>, S: NetworkStream + Send> NetworkConnector
+    for DnsConnector<C> {
     type Stream = S;
 
     /// Performs DNS SRV resolution, then calls into the inner connector with the results.
@@ -36,11 +37,17 @@ impl <C: hyper::net::NetworkConnector<Stream=S>, S: hyper::net::NetworkStream + 
     /// It just takes a random entry in the DNS answers that are returned.
     fn connect(&self, host: &str, _port: u16, scheme: &str) -> hyper::Result<S> {
 
-        let mut io = tokio_core::reactor::Core::new().expect("Failed to create event loop for DNS query");
+        let mut io =
+            tokio_core::reactor::Core::new().expect("Failed to create event loop for DNS query");
         let (stream, sender) = trust_dns::udp::UdpClientStream::new(self.dns_addr, io.handle());
-        let mut dns_client = trust_dns::client::ClientFuture::new(stream, sender, io.handle(), None);
+        let mut dns_client =
+            trust_dns::client::ClientFuture::new(stream, sender, io.handle(), None);
 
-        let name = trust_dns::rr::Name::parse(host, None).unwrap();
+        // TODO: Check if this is a domain name or not before trying to use
+        // DNS resolution.
+
+        // Add a `.` to the end of the host so that we can query the domain records.
+        let name = trust_dns::rr::Name::parse(&format!("{}.", host), None).unwrap();
 
         match io.run(dns_client.query(name,
                                       trust_dns::rr::DNSClass::IN,
@@ -49,20 +56,56 @@ impl <C: hyper::net::NetworkConnector<Stream=S>, S: hyper::net::NetworkStream + 
                 let answers = res.get_answers();
 
                 if answers.is_empty() {
-                    return Err(std::io::Error::new(std::io::ErrorKind::Other, "No valid DNS answers").into())
+                    return Err(std::io::Error::new(std::io::ErrorKind::Other,
+                                                   "No valid DNS answers")
+                                       .into());
                 }
 
                 let mut rng = rand::thread_rng();
-                let answer = rng.choose(answers).expect("Sort out what to return here");
+                let answer = rng.choose(answers)
+                    .expect("Sort out what to return here");
 
                 let srv = match *answer.get_rdata() {
                     trust_dns::rr::RData::SRV(ref srv) => srv,
-                    _ => return Err(std::io::Error::new(std::io::ErrorKind::Other, "Unexpected DNS response").into()),
+                    _ => {
+                        return Err(std::io::Error::new(std::io::ErrorKind::Other,
+                                                       "Unexpected DNS response")
+                                           .into())
+                    }
                 };
 
-                self.connector.connect(&srv.get_target().to_string(), srv.get_port(), scheme)
-            },
-            _ => Err(std::io::Error::new(std::io::ErrorKind::Other, "Failed to query DNS server").into()),
+                let target = srv.get_target();
+
+                // Now need to lookup the target in the additional information
+                let additionals = res.get_additionals();
+
+                let entry = additionals
+                    .iter()
+                    .find(|additional| additional.get_name() == target);
+
+                if let Some(entry) = entry {
+                    let addr = match *entry.get_rdata() {
+                        trust_dns::rr::RData::A(ref addr) => addr,
+                        _ => {
+                            return Err(std::io::Error::new(std::io::ErrorKind::Other,
+                                                           "Did not receive a valid record")
+                                               .into())
+                        }
+                    };
+
+                    self.connector
+                        .connect(&addr.to_string(), srv.get_port(), scheme)
+                } else {
+                    Err(std::io::Error::new(std::io::ErrorKind::Other,
+                                            "Did not receive a valid record")
+                                .into())
+                }
+
+            }
+            _ => {
+                Err(std::io::Error::new(std::io::ErrorKind::Other, "Failed to query DNS server")
+                        .into())
+            }
         }
     }
 }
@@ -70,6 +113,5 @@ impl <C: hyper::net::NetworkConnector<Stream=S>, S: hyper::net::NetworkStream + 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-    }
+    fn it_works() {}
 }
